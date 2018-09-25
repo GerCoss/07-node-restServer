8b37e542cabb01a500b63526ec478866822f71c8
GerCoss <gerardocoss92@gmail.com>
2018-09-25 01:46:01 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,22 @@
+// ==========================
+//  Puerto
+// ==========================
 //process es un objeto global que corre a lo largo de la aplicacion de node y se actualiza dependiendo del enviroment
 process.env.PORT = process.env.PORT || 8081;
+
+// ==========================
+//  Entorno
+// ==========================
+//para saber si estoy en produccion
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ==========================
+//  Base de datos
+// ==========================
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:5535668694ger@ds263710.mlab.com:63710/cafe56'
+}
+process.env.URLDB = urlDB;
